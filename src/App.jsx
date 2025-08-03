4e494766670ca1a1867ba52e12382c7a5976371a
Andrej Trožić <andrejtrozic95@gmail.com>
2025-08-03 22:16:50 +0200

    Update App.jsx
    App.jsx has been updated and is ready for testing.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import MainLayout from "./layouts/MainLayout";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import AboutPage from "./pages/AboutPage";
 
 const HomePage = () => (
   <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 py-10">
@@ -38,6 +39,7 @@ const App = () => {
       <Route path="/sign-up/*" element={<SignUpPage />} />
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
       </Route>
     </Routes>
   );
