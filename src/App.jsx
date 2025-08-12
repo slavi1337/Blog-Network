9c94295d57924997e08242a1f47b024e5bdad818
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-13 00:46:52 +0200

    mini izmjene + edit post ruta

--- src/App.jsx
+++ src/App.jsx
@@ -18,37 +18,33 @@ import AboutPage from "./pages/AboutPage";
 
 const HomePage = () => (
   <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 py-10">
-    <div className="">
-      <h1 className="text-4xl font-bold">Dobrodošli na Blog Network!</h1>
-      <p className="mt-4">
-        Ovo je početna stranica. Izaberite opciju iz navigacije ili započnite sa
-        kreiranjem!
-      </p>
+    <div className="flex flex-row justify-between border-b-2 border-gray-300">
+      <div className="">
+        <h1 className="text-4xl font-bold">Dobrodošli na Blog Network!</h1>
+        <p className="mt-4">
+          Ovo je početna stranica. Izaberite opciju iz navigacije ili započnite
+          sa kreiranjem!
+        </p>
+      </div>
+
+      <div className="text-center md:text-right mb-12">
+        <SignedIn>
+          <Link to="/create-blog">
+            <button className="py-3 px-6 rounded-lg bg-orange-500 hover:bg-orange-600 text-white font-bold text-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">
+              Kreiraj Novu Objavu
+            </button>
+          </Link>
+        </SignedIn>
+
+        <SignedOut>
+          <Link to="/sign-up">
+            <button className="py-3 px-6 rounded-lg bg-orange-500 hover:bg-orange-600 text-white font-bold text-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">
+              Pridruži se i Kreiraj Svoj Blog!
+            </button>
+          </Link>
+        </SignedOut>
+      </div>
     </div>
-
-    <div className="text-center md:text-right mb-8">
-      <SignedIn>
-        <Link to="/create-blog">
-          <button className="py-3 px-6 rounded-lg bg-orange-500 hover:bg-orange-600 text-white font-bold text-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">
-            Kreiraj Novu Objavu
-          </button>
-        </Link>
-      </SignedIn>
-
-      <SignedOut>
-        <Link to="/sign-up">
-          <button className="py-3 px-6 rounded-lg bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">
-            Pridruži se i Kreiraj Svoj Blog!
-          </button>
-        </Link>
-      </SignedOut>
-    </div>
-
-    <h1 className="text-3xl font-bold">Dobrodošli na Blog Network!</h1>
-    <p className="mt-4">
-      Ovo je početna stranica. Izaberite opciju iz navigacije ili započnite sa
-      kreiranjem!
-    </p>
 
     <div className="mt-8 text-center">
       <h1 className="text-3xl font-bold text-orange-600 mb-6">
@@ -156,14 +152,19 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
 
+        <Route path="edit-post/:slug" element={<BlogCreationPage />} />
+
         <Route path="create-blog" element={<BlogCreationPage />} />
+
         <Route path="about" element={<AboutPage />} />
+
         <Route path="profile/:username" element={<ProfilePage />}>
           <Route index element={<ProfilePosts />} />
           <Route path="saved" element={<SavedPosts />} />
           <Route path="history" element={<ReadingHistory />} />
           <Route path="edit" element={<EditProfilePage />} />
         </Route>
+
         <Route path="/posts/:slug" element={<SinglePostPage />} />
       </Route>
     </Routes>
