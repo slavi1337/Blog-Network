4be679667431062b364073c5c56bd1c793a4b984
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-13 00:43:22 +0200

    promjena teksta u zavisnosti jel profil moj ili tudji

--- src/components/ProfilePosts.jsx
+++ src/components/ProfilePosts.jsx
@@ -2,11 +2,13 @@ import { useOutletContext } from "react-router-dom";
 import PostList from "./PostList";
 
 const ProfilePosts = () => {
-  const { posts } = useOutletContext();
+  const { posts, isOwnProfile } = useOutletContext();
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-6">Moje Objave</h2>
+      <h2 className="text-2xl font-bold mb-6">
+        {isOwnProfile ? "Moje Objave" : "Objave"}
+      </h2>
       <PostList posts={posts} />
     </div>
   );
