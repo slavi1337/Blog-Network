379d9c9ba485fb00069d891a5ccaa00d34fa55a2
Andrej Trožić <andrejtrozic95@gmail.com>
2025-08-12 19:04:41 +0200

    Update tailwind.config.js
    tailwind.config.js has been updated and ready for testing.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        primary_accent: "var(--color-primary-accent)",
+        background: "var(--color-background)",
+        textcolor: "var(--color-textcolor)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
