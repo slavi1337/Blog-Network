178fad6017422bd7bd129a4e5e84689c8a221786
Andrej Trožić <andrejtrozic95@gmail.com>
2025-08-12 19:02:21 +0200

    Update App.jsx
    App.jsx has been updated and ready for testing.

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,11 @@ import ProfilePosts from "./components/ProfilePosts";
 import SavedPosts from "./pages/SavedPostsPage";
 import ReadingHistory from "./pages/ReadingHistoryPage";
 import BlogCreationPage from "./pages/BlogCreationPage";
-import PostOfTheWeek from "./components/PostOfTheWeek"; 
+import PostOfTheWeek from "./components/PostOfTheWeek";
 import SinglePostPage from "./pages/SinglePostPage";
-
+import AboutPage from "./pages/AboutPage";
 
 const HomePage = () => (
-
   <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 py-10">
     <div className="">
       <h1 className="text-4xl font-bold">Dobrodošli na Blog Network!</h1>
@@ -52,9 +51,11 @@ const HomePage = () => (
     </p>
 
     <div className="mt-8 text-center">
-      <h1 className="text-3xl font-bold text-orange-600 mb-6">Objava sedmice!</h1>
+      <h1 className="text-3xl font-bold text-orange-600 mb-6">
+        Objava sedmice!
+      </h1>
       <div className="flex flex-row border-4 border-orange-500 p-4 mt-4">
-        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8"/>
+        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8" />
         <div className="text-center w-3/4">
           <div className="mb-4">
             <h1 className="font-bold text-xl">Naslov bloga</h1>
@@ -64,7 +65,12 @@ const HomePage = () => (
             <span className="text-gray-500 mr-4">-</span>
             <span className="text-gray-500 mr-4">kategorija</span>
           </div>
-          <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quasi vitae tempore velit provident. Nemo eligendi deserunt eaque vero, cupiditate similique, a ea repudiandae neque amet nisi eius consectetur doloribus!</p>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            quasi vitae tempore velit provident. Nemo eligendi deserunt eaque
+            vero, cupiditate similique, a ea repudiandae neque amet nisi eius
+            consectetur doloribus!
+          </p>
         </div>
       </div>
     </div>
@@ -74,45 +80,60 @@ const HomePage = () => (
         Najnovije objave
       </h2>
       <div className="flex flex-row border-b-2 border-gray-300 pb-4 mt-4">
-        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8"/>
+        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8" />
         <div className="text-center w-3/4">
           <div className="mb-4">
-           <h1 className="font-bold text-xl">Naslov bloga</h1>
-           <span className="text-gray-500 mr-4">ime korisnika</span>
-           <span className="text-gray-500 mr-4">-</span>
-           <span className="text-gray-500 mr-4">vrijeme objave</span>
-           <span className="text-gray-500 mr-4">-</span>
-           <span className="text-gray-500 mr-4">kategorija</span>
+            <h1 className="font-bold text-xl">Naslov bloga</h1>
+            <span className="text-gray-500 mr-4">ime korisnika</span>
+            <span className="text-gray-500 mr-4">-</span>
+            <span className="text-gray-500 mr-4">vrijeme objave</span>
+            <span className="text-gray-500 mr-4">-</span>
+            <span className="text-gray-500 mr-4">kategorija</span>
           </div>
-          <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quasi vitae tempore velit provident. Nemo eligendi deserunt eaque vero, cupiditate similique, a ea repudiandae neque amet nisi eius consectetur doloribus!</p>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            quasi vitae tempore velit provident. Nemo eligendi deserunt eaque
+            vero, cupiditate similique, a ea repudiandae neque amet nisi eius
+            consectetur doloribus!
+          </p>
         </div>
       </div>
       <div className="flex flex-row border-b-2 border-gray-300 pb-4 mt-4">
-        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8"/>
+        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8" />
         <div className="text-center w-3/4">
           <div className="mb-4">
-           <h1 className="font-bold text-xl">Naslov bloga</h1>
-           <span className="text-gray-500 mr-4">ime korisnika</span>
-           <span className="text-gray-500 mr-4">-</span>
-           <span className="text-gray-500 mr-4">vrijeme objave</span>
-           <span className="text-gray-500 mr-4">-</span>
-           <span className="text-gray-500 mr-4">kategorija</span>
+            <h1 className="font-bold text-xl">Naslov bloga</h1>
+            <span className="text-gray-500 mr-4">ime korisnika</span>
+            <span className="text-gray-500 mr-4">-</span>
+            <span className="text-gray-500 mr-4">vrijeme objave</span>
+            <span className="text-gray-500 mr-4">-</span>
+            <span className="text-gray-500 mr-4">kategorija</span>
           </div>
-          <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quasi vitae tempore velit provident. Nemo eligendi deserunt eaque vero, cupiditate similique, a ea repudiandae neque amet nisi eius consectetur doloribus!</p>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            quasi vitae tempore velit provident. Nemo eligendi deserunt eaque
+            vero, cupiditate similique, a ea repudiandae neque amet nisi eius
+            consectetur doloribus!
+          </p>
         </div>
       </div>
       <div className="flex flex-row border-b-2 border-gray-300 pb-4 mt-4">
-        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8"/>
+        <img src="public/vite.svg" alt="Image" className="w-1/4 mr-8" />
         <div className="text-center w-3/4">
           <div className="mb-4">
-           <h1 className="font-bold text-xl">Naslov bloga</h1>
-           <span className="text-gray-500 mr-4">ime korisnika</span>
-           <span className="text-gray-500 mr-4">-</span>
-           <span className="text-gray-500 mr-4">vrijeme objave</span>
-           <span className="text-gray-500 mr-4">-</span>
-           <span className="text-gray-500 mr-4">kategorija</span>
+            <h1 className="font-bold text-xl">Naslov bloga</h1>
+            <span className="text-gray-500 mr-4">ime korisnika</span>
+            <span className="text-gray-500 mr-4">-</span>
+            <span className="text-gray-500 mr-4">vrijeme objave</span>
+            <span className="text-gray-500 mr-4">-</span>
+            <span className="text-gray-500 mr-4">kategorija</span>
           </div>
-          <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quasi vitae tempore velit provident. Nemo eligendi deserunt eaque vero, cupiditate similique, a ea repudiandae neque amet nisi eius consectetur doloribus!</p>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            quasi vitae tempore velit provident. Nemo eligendi deserunt eaque
+            vero, cupiditate similique, a ea repudiandae neque amet nisi eius
+            consectetur doloribus!
+          </p>
         </div>
       </div>
     </div>
@@ -136,7 +157,7 @@ const App = () => {
         <Route index element={<HomePage />} />
 
         <Route path="create-blog" element={<BlogCreationPage />} />
-
+        <Route path="about" element={<AboutPage />} />
         <Route path="profile/:username" element={<ProfilePage />}>
           <Route index element={<ProfilePosts />} />
           <Route path="saved" element={<SavedPosts />} />
