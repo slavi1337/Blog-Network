5ca55e563be55e0cecf76818322ca0888e27411d
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-14 01:10:25 +0200

    dodane ikonice za notifikacije on/off na profilu

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,6 +3,47 @@ import { useUser, useAuth } from "@clerk/clerk-react";
 import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 import PostList from "../components/PostList";
 
+const BellOnIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 text-orange-500"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+    />
+  </svg>
+);
+
+const BellOffIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 text-gray-400"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M6 18L18 6M6 6l12 12"
+    />
+
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M13.586 3.586A2 2 0 1115 5v4.586m-3.207 4.207a2 2 0 01-2.364.386l-1.42-1.42A2 2 0 016 13.172V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341"
+    />
+  </svg>
+);
+
 const ProfilePage = () => {
   const { username: profileUsername } = useParams();
   const { user: loggedInUser, isLoaded, isSignedIn } = useUser();
@@ -100,9 +141,8 @@ const ProfilePage = () => {
       !window.confirm(
         `Da li ste sigurni da želite da blokirate korisnika @${profileData.username}? Nećete moći da vidite njegove objave i komentare, i biće automatski otpraćen.`
       )
-    ) {
+    )
       return;
-    }
 
     if (newBlockState && isFollowing) {
       setProfileData((prev) => ({
@@ -187,7 +227,7 @@ const ProfilePage = () => {
           </div>
 
           {isSignedIn && !isOwnProfile && (
-            <div className="mt-4 flex justify-center md:justify-start gap-3">
+            <div className="mt-4 flex justify-center md:justify-start items-center gap-3">
               {!isBlocked && (
                 <button
                   onClick={handleFollowToggle}
