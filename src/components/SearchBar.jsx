366ab5a1ea19628a6592cadefa466a06b923a302
Og?en <glassglove17@gmail.com>
2025-08-14 18:24:52 +0200

    Implementacija komponente pretrage

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button type="submit">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
