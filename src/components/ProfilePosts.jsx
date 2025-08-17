f6bb99edacf1190b3d8ddf8b20d630ccfb69ac4a
Og?en <glassglove17@gmail.com>
2025-08-17 20:30:56 +0200

    Dodane poruke za debug

--- src/components/ProfilePosts.jsx
+++ src/components/ProfilePosts.jsx
@@ -4,6 +4,11 @@ import PostList from "./PostList";
 const ProfilePosts = () => {
   const { posts, isOwnProfile } = useOutletContext();
 
+  console.log("Podaci stigli u ProfilePosts:", posts);
+  if (posts && posts.length > 0) {
+    console.log("Prvi post u ProfilePosts:", posts[0]);
+  }
+
   return (
     <div>
       <h2 className="text-2xl font-bold mb-6">
