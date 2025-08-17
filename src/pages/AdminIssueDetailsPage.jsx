c62ad64f45748b6c36eec35cd306688d72afe03f
Andrej Trožić <andrejtrozic95@gmail.com>
2025-08-17 17:12:42 +0200

    Create AdminIssueDetailsPage.jsx

---
+++ src/pages/AdminIssueDetailsPage.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+
+const AdminIssueDetailsPage = () => {
+  
+};
+
+export default AdminIssueDetailsPage;
