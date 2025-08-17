b25638e57a58b355a4581ba832edf002953fbcb4
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-17 18:39:23 +0200

    dodan blog of the week frontend

--- src/App.jsx
+++ src/App.jsx
@@ -102,44 +102,7 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="mt-8 text-center">
-        <h1 className="text-3xl font-bold text-primary mb-6">
-          Objava sedmice!
-        </h1>
-        <div className="flex flex-row border-4 border-primary p-4 mt-4">
-          <img src="/vite.svg" alt="Image" className="w-1/4 mr-8" />
-          <div className="text-center w-3/4">
-            <div className="mb-4">
-              <h1 className="font-bold text-xl">Naslov bloga</h1>
-              <span className="text-gray-500 mr-4">ime korisnika</span>
-              <span className="text-gray-500 mr-4">-</span>
-              <span className="text-gray-500 mr-4">vrijeme objave</span>
-              <span className="text-gray-500 mr-4">-</span>
-              <span className="text-gray-500 mr-4">kategorija</span>
-            </div>
-            <p className="">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Accusamus quasi vitae tempore velit provident. Nemo eligendi
-              deserunt eaque vero, cupiditate similique, a ea repudiandae neque
-              amet nisi eius consectetur doloribus!
-            </p>
-          </div>
-        </div>
-
-        <InfiniteScroll
-          dataLength={posts.length}
-          next={() => fetchPosts(page)}
-          hasMore={hasMore}
-          loader={<h4 className="text-center text-gray-500">Učitavanje...</h4>}
-          endMessage={
-            <p className="text-center text-gray-400 mt-4">
-              <b>Učitali ste sve objave za date parametre.</b>
-            </p>
-          }
-        >
-          <PostList posts={posts} />
-        </InfiniteScroll>
-      </div>
+      <PostOfTheWeek />
     </div>
   );
 };
