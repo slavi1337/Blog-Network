a98e7438a2b5bc5e5519a4955f3c24b6fa82d3d2
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2025-08-21 12:08:34 +0200

    mala izmjena

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -122,7 +122,7 @@ const postsRouter = (pool, getInternalUserId) => {
       await client.query("COMMIT");
 
       res.status(201).json({
-        message: `Post uspješno sačuvan kao ${finalStatus}!`,
+        message: `Blog uspješno sačuvan kao ${finalStatus}!`,
         post: newPost,
       });
 
